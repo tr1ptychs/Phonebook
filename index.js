21d07b6f7a21d1f087eabc9b35e28dd55948c651
Wyatt <wyatt.furois@gmail.com>
2024-07-19 02:12:32 +0200

    get, delete, post added

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.use(express.json())
 
-const persons = [
+let persons = [
     {
         "id": "1",
         "name": "Arto Hellas",
@@ -26,10 +26,72 @@ const persons = [
     }
 ]
 
-app.get('/persons/', (request, response) => {
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people<br/>
+        <br/>${Date()}</p>`)
+})
+
+app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(note => note.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(n => Number(n.id)))
+        : 0
+    return String(maxId + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    console.log(body)
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+
+    if (persons.find(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'Name must be unique'
+        })
+    }
+
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
